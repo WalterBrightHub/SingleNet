d534d989d89b0bf90a748f12386738bda196c46a
WalterBright <ssmbuild@outlook.com>
2018-05-29 00:00:16 +0800

    完成基础功能

---
+++ app/src/main/java/com/example/ssmbu/singlenet/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.ssmbu.singlenet;
+
+import android.Manifest;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.telephony.SubscriptionInfo;
+import android.telephony.SubscriptionManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btn_sendMM;
+    private RadioButton sim1,sim2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //运行时权限，发送短信
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.SEND_SMS}, 1);
+        }
+
+        sim1=(RadioButton)findViewById(R.id.sim1);
+        sim2=(RadioButton)findViewById(R.id.sim2);
+
+
+        final SmsManager smsManager=SmsManager.getDefault();
+        btn_sendMM=(Button)findViewById(R.id.btn_sendMM);
+        btn_sendMM.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String number="106593005";
+                String text="mm";
+                PendingIntent pi = PendingIntent.getActivity(MainActivity.this,0,new Intent(),0);
+                smsManager.sendTextMessage(number,null,text,pi,null);
+
+            }
+        });
+        //btn_sendMM.click
+    }
+}
---
+++ app/src/main/java/com/example/ssmbu/singlenet/NewAppWidget.java
@@ -0,0 +1,64 @@
+package com.example.ssmbu.singlenet;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+import android.widget.Toast;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class NewAppWidget extends AppWidgetProvider {
+
+     public static final String CLICK_ACTION="com.example.ssmbu.singlenet.action.CLICK";
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+
+        CharSequence widgetText = context.getString(R.string.appwidget_text);
+        // Construct the RemoteViews object
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
+        //views.setTextViewText(R.id.appwidget_text, widgetText);
+
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+
+        Intent intent = new Intent(CLICK_ACTION);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, R.id.yinxian_imageView, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        views.setOnClickPendingIntent(R.id.yinxian_imageView, pendingIntent);
+
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+        if (CLICK_ACTION.equals(intent.getAction())){
+            //Toast.makeText(context, "hello dog!", Toast.LENGTH_SHORT).show();
+            //MainActivity.
+
+        }
+    }
+}
+
